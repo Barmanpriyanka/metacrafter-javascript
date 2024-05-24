217e2a31f34dfde47139ebd685c65ae17576c662
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-05-24 19:56:27 +0530

    Learning about variable in javascript

--- meta.js
+++ meta.js
@@ -7,7 +7,34 @@ multi-line comment */
 
 var myName="priyanka barman";
 console.log(myName);
-myVariable = 5;
+ var myVariable = 5;
 console.log(myVariable);
+
+// declares a myVar variable with no value, then assigns it the value 5. Next, a variable named myNum is declared with no value. Then, the contents of myVar (which is 5) is assigned to the variable myNum. Now, myNum also has the value of 5.
+
+var myVar;
+myVar = 5;
+var myNum;
+myNum = myVar;
+console.log(myNum);
+
+//Creates a new variable called myVar and assigns it an initial value of 0.
+var myVar = 0;
+
+//When JavaScript variables are declared, they have an initial value of undefined. If you do a mathematical operation on an undefined variable, your result will be NaN, which means "Not a Number." If you concatenate a string with an undefined variable, you will get a string of undefined.
+//Write variable names in JavaScript in camelCase. In camelCase, multi-word variable names have the first word in lowercase and the first letter of each subsequent word is capitalized.
+
+var someVariable;
+var anotherVariableName;
+var thisVariableNameIsSoLong;
+
+//One of the biggest problems with declaring variables with the var keyword is that you can easily overwrite variable declarations:
+var camper = "James";
+var camper = "David";
+console.log(camper);//it will answer as david as overwrite it .To solve this problem we use let.
+//If  we  replace var with let in the code above, it results in an error:
+let camper = "James";
+let camper = "David";
+
 
 

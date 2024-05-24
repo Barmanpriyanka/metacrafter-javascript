5a8a286c89fd35683b7de6e8db7594aedda12c47
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-05-24 19:38:43 +0530

    first commit

---
+++ meta.js
@@ -0,0 +1,13 @@
+// This is an in-line comment.
+
+/* This is a
+multi-line comment */
+
+/*creates a variable called myName. In JavaScript, we end statements with semicolons. Variable names can be made up of numbers, letters, and $ or _, but may not contain spaces or start with a number.*/
+
+var myName="priyanka barman";
+console.log(myName);
+myVariable = 5;
+console.log(myVariable);
+
+

89fd1a46eca5a2c48ba9e61d5aed4e4ce85fe745
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-05-24 21:51:24 +0530

    Understanding about operator in javascript

---
+++ understandingOperator.js
@@ -0,0 +1,43 @@
+//Subtract One Number from Another with JavaScript
+let myVar = 12 - 6;
+console.log("subtraction is :",myVar);//myVar would have the value 6.
+
+//Multiply Two Numbers with JavaScript
+let myVar2 = 13 * 13;
+console.log("multiplication is:",myVar2);//169  
+
+//Divide One Number by Another with JavaScript
+let myVar3 = 16 / 2;
+console.log("division is:",myVar3);//8
+//Increment a Number with JavaScript
+let i=5;
+i = i + 1;
+console.log(i);//6
+
+//Decrement a Number with JavaScript
+i--;
+console.log(i);//5
+//multiply two decimal number
+let a=2.72* 7.64;
+console.log(" multiply of decimal number is : ",a);
+//divide one decimal with another
+let b=1.6/2.6;
+console.log(b); 
+//Finding a Remainder in JavaScript
+let remainder=5%2;
+console.log("remainder is :",remainder);//% operator gives reamainder
+
+//Compound Assignment With Augmented Addition
+let myVar4= 1;
+myVar4 += 5;// myVar = myVar+5 is same;
+console.log("Compound Assignment With Augmented Addition: ",myVar4);//6
+//Compound Assignment With Augmented Subtraction
+myVar4 -= 5;
+console.log("Compound Assignment With Augmented Subtraction: ",myVar4);
+
+
+
+
+
+
+

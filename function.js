24939dec0ff4dba867c16ed24501d09ac0f04492
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-06-02 00:09:00 +0530

    for loop , while loop and parseInt function

--- function.js
+++ function.js
@@ -53,7 +53,62 @@ console.log(equal);
 function myFun() {
     console.log("Hello");
     return "World";
-    console.log("byebye")
+    console.log("byebye")//he string byebye will never display in the console because the function exits at the return statement.
   }
   myFun();
-  
+  //Iterate with JavaScript While Loops
+  const ourArray = [];
+let i = 0;
+
+while (i < 5) {
+  ourArray.push(i);
+  console.log(i);
+  i++;
+}
+//Iterate Odd Numbers With a For Loop
+const ourArray2 = [];
+
+for (let i = 0; i < 10; i += 2) {
+  ourArray2.push(i);
+  console.log(i);
+}
+//Count Backwards With a For Loop
+const ourArray3 = [];
+
+for (let i = 10; i > 0; i -= 2) {
+  ourArray2.push(i);
+  console.log(i);
+}
+
+
+//Iterate Through an Array with a For Loop
+const arr = [10, 9, 8, 7, 6];
+
+for (let i = 0; i < arr.length; i++) {
+   console.log(arr[i]);
+}
+//Nesting For Loops
+const arr2 = [
+  [1, 2], [3, 4], [5, 6]
+];
+
+for (let i = 0; i < arr2.length; i++) {
+  for (let j = 0; j < arr2[i].length; j++) {
+    console.log("nesting loops:",arr2[i][j]);
+  }
+}
+//Iterate with JavaScript Do...While Loops
+const ourArray4 = [];
+let j = 0;
+
+do {
+  ourArray4.push(j);
+  j++;
+} while (j < 5);
+//a do...while loop ensures that the code inside the loop will run at least once.
+
+
+//Use the parseInt Function
+//The parseInt() function parses a string and returns an integer. 
+const a = parseInt("007");
+//if a string cnnnot be converted to the integer then a it return NaN
